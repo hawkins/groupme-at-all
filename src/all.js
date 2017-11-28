795a1cfb93b51bfbf36ce09c1e4f584de36fd69e
Josh Hawkins <jkhaccounts@gmail.com>
2017-11-28 11:21:42 -0600

    Refactor run function
    Still not sold on this being a class. There will only ever be one... why
    not just a function?

--- src/all.js
+++ src/all.js
@@ -69,132 +69,137 @@ class AllBot {
     else return user;
   }
 
+  respondToID(res, target) {
+    // Get ID command
+    console.log(`Looking for user ID by name: ${target}`);
+    const found = this.getUserByName(target);
+
+    if (found) {
+      const id = found.user_id;
+      console.log(`Found ID ${id} by name ${target}`);
+      res.send(`${target}: ${id}`);
+    } else {
+      res.send(`Could not find a user with the name ${target}`);
+    }
+  }
+
+  respondToName(res, target) {
+    console.log(`Looking for user name by ID: ${target}`);
+    const found = this.getUserById(target);
+
+    if (found) {
+      const name = found.name;
+      console.log(`Found name ${name} by ID ${target}`);
+      res.send(`${target}: ${name}`);
+    } else {
+      res.send(`Could not find a user with the ID ${target}`);
+    }
+  }
+
+  respondToViewBlacklist(res) {
+    // Raw blacklist
+    if (res.match[1]) return res.send(JSON.strinify(this.blacklist));
+
+    const blacklistNames = this.blacklist.map(
+      user => this.getUserById(id).name
+    );
+
+    if (blacklistNames.length > 0) return res.send(blacklistNames.join(", "));
+    else return res.send("There are currently no users blacklisted.");
+  }
+
+  respondToBlacklist(res, target) {
+    const user = this.getUserByName(target);
+
+    if (!user) return res.send(`Could not find a user with the name ${target}`);
+
+    conosle.log(`Blacklisting ${target}, ${user.user_id}`);
+    this.addToBlacklist(user.user_id);
+    res.send(`Blacklisted ${target} successfully.`);
+  }
+
+  respondToWhitelist(res, target) {
+    const user = this.getUserByName(target);
+
+    if (!user) return res.send(`Could not find a user with the name ${target}`);
+
+    console.log(`Whitelisting ${target}, ${user.user_id}`);
+    this.removeFromBlacklist(user.user_id);
+    res.send(`Whitelisted ${target} successfully`);
+  }
+
+  respondToAtAll(res) {
+    // Select the longer of the two options.
+    // TODO: Maybe combine them?
+    const text =
+      res.match[0].length > res.match[1].length ? res.match[0] : res.match[1];
+
+    // Default text if not long enough
+    // TODO: Is this necessary? Can't we tag everyone on a 1 character message?
+    // if (text.length < users.length)
+    //   text = "Please check the GroupMe, everyone.";
+
+    // The message for use in GroupMe API
+    const message = {
+      text,
+      bot_id,
+      attachments: [{ loci: [], type: "mentions", user_ids: [] }]
+    };
+
+    // Add "mention" for each user
+    const users = this.robot.brain.users();
+    Object.keys(users).map((userID, index) => {
+      // Skip blacklisted users
+      if (this.blacklist.indexOf(userID) !== -1) return;
+
+      // TODO: Would [i, i] work?
+      message.attachments[0].loci.push([index, index + 1]);
+      message.attachments[0].user_ids.push(userID);
+    });
+
+    // Send the request
+    const json = JSON.stringify(message);
+    const groupmeAPIOptions = {
+      agent: false,
+      host: "api.groupme.com",
+      path: "/v3/bots/post",
+      port: 443,
+      method: "POST",
+      headers: {
+        "Content-Length": json.length,
+        "Content-Type": "application/json",
+        "X-Access-Token": token
+      }
+    };
+    const req = https.request(groupmeAPIOptions, response => {
+      let data = "";
+      response.on("data", chunk => (data += chunk));
+      response.on("end", () =>
+        console.log(`[GROUPME RESPONSE] ${response.statusCode} ${data}`)
+      );
+    });
+    req.end(json);
+  }
+
   // Defines the main logic of the bot
   run() {
-    // TODO: Extract these anonymous functions into their own spot somewhere
-
-    // Get ID command
-    this.robot.hear(/get id (.+)/i, res => {
-      const target = res.match[1];
-      console.log(`Looking for user ID by name: ${target}`);
-      const found = this.getUserByName(target);
-
-      if (found) {
-        const id = found.user_id;
-        console.log(`Found ID ${id} by name ${target}`);
-        res.send(`${target}: ${id}`);
-      } else {
-        res.send(`Could not find a user with the name ${target}`);
-      }
-    });
-
-    // Get Name command
-    this.robot.hear(/get name (.+)/i, res => {
-      const target = res.match[1];
-      console.log(`Looking for user name by ID: ${target}`);
-      const found = this.getUserById(target);
-
-      if (found) {
-        const name = found.name;
-        console.log(`Found name ${name} by ID ${target}`);
-        res.send(`${target}: ${name}`);
-      } else {
-        res.send(`Could not find a user with the ID ${target}`);
-      }
-    });
-
-    // View blacklist command
-    this.robot.hear(/view( raw)* blacklist/i, res => {
-      // Raw blacklist
-      if (res.match[1]) return res.send(JSON.strinify(this.blacklist));
-
-      const blacklistNames = this.blacklist.map(
-        user => this.getUserById(id).name
-      );
-
-      if (blacklistNames.length > 0) return res.send(blacklistNames.join(", "));
-      else return res.send("There are currently no users blacklisted.");
-    });
-
-    // Blacklist command
-    this.robot.hear(/blacklist (.+)/i, res => {
-      const target = res.match[1];
-      const user = this.getUserByName(target);
-
-      if (!user)
-        return res.send(`Could not find a user with the name ${target}`);
-
-      conosle.log(`Blacklisting ${target}, ${user.user_id}`);
-      this.addToBlacklist(user.user_id);
-      res.send(`Blacklisted ${target} successfully.`);
-    });
-
-    // Whitelist command
-    this.robot.hear(/whitelist (.+)/i, res => {
-      const target = res.match[1];
-      const user = this.getUserByName(target);
-
-      if (!user)
-        return res.send(`Could not find a user with the name ${target}`);
-
-      console.log(`Whitelisting ${target}, ${user.user_id}`);
-      this.removeFromBlacklist(user.user_id);
-      res.send(`Whitelisted ${target} successfully`);
-    });
+    // Register listeners with hubot
+    this.robot.hear(/get id (.+)/i, res => this.respondToID(res, res.match[1]));
+    this.robot.hear(/get name (.+)/i, res =>
+      this.respondToName(res, res.match[1])
+    );
+    this.robot.hear(/view( raw)* blacklist/i, res =>
+      this.respondToViewBlacklist(res)
+    );
+    this.robot.hear(/blacklist (.+)/i, res =>
+      this.respondToBlacklist(res, res.match[1])
+    );
+    this.robot.hear(/whitelist (.+)/i, res =>
+      respondToWhitelist(res, res.match[1])
+    );
 
     // Mention @all command
-    this.robot.hear(/(.*)@all(.*)/i, res => {
-      // Select the longer of the two options.
-      // TODO: Maybe combine them?
-      const text =
-        res.match[0].length > res.match[1].length ? res.match[0] : res.match[1];
-      const users = this.robot.brain.users();
-
-      // Default text if not long enough
-      // TODO: Is this necessary? Can't we tag everyone on a 1 character message?
-      // if (text.length < users.length)
-      //   text = "Please check the GroupMe, everyone.";
-
-      // The message for use in GroupMe API
-      let message = {
-        text,
-        bot_id,
-        attachments: [{ loci: [], type: "mentions", user_ids: [] }]
-      };
-
-      // Add "mention" for each user
-      Object.keys(users).map((userID, index) => {
-        // Skip blacklisted users
-        if (this.blacklist.indexOf(userID) !== -1) return;
-
-        // TODO: Would [i, i] work?
-        message.attachments[0].loci.push([index, index + 1]);
-        message.attachments[0].user_ids.push(userID);
-      });
-
-      // Send the request
-      const json = JSON.stringify(message);
-      const groupmeAPIOptions = {
-        agent: false,
-        host: "api.groupme.com",
-        path: "/v3/bots/post",
-        port: 443,
-        method: "POST",
-        headers: {
-          "Content-Length": json.length,
-          "Content-Type": "application/json",
-          "X-Access-Token": token
-        }
-      };
-      const req = https.request(groupmeAPIOptions, response => {
-        let data = "";
-        response.on("data", chunk => (data += chunk));
-        response.on("end", () =>
-          console.log(`[GROUPME RESPONSE] ${response.statusCode} ${data}`)
-        );
-      });
-      req.end(json);
-    });
+    this.robot.hear(/(.*)@all(.*)/i, res => this.respondToAtAll(res));
   }
 }
 
